d0028d4cb25f4e8b276590a2e81fae9e98c82f88
Nicolas Cabrera <nicolascabreramillacan@gmail.com>
2023-10-10 18:03:56 -0300

    feat: add userId.js

---
+++ js/Data/userId.js
@@ -0,0 +1,66 @@
+export const PropertyData = {CodigoUsuarioMaestro: 0, companyId: 1, realtorId:0};
+
+
+export const limitDataApi = { limit:6, limitMap: 99 };
+
+
+export const RealtorSendEmailData = {
+    sendEmail: 'nicolascabreramillacan@gmail.com',  /* SendEmail.js */
+    detail: 'nicolascabreramillacan@gmail.com',     /* SendEmailDetail.js */
+    public: 'nicolascabreramillacan@gmail.com',     /* SendEmailPublic.js */
+};
+
+export const AboutInformation = {
+    mision: 'Data-Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis officiis adipisci ullam similique unde voluptates rerum sed eligendi, qui itaque laboriosam nihil asperiores? Voluptate maiores error eius assumenda distinctio esse.',
+    vision: 'Data-Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae nostrum delectus nesciunt reprehenderit consequuntur labore, a ratione velit, sint mollitia quo ducimus fugit nulla animi praesentium placeat dolorum natus architecto? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae nostrum delectus nesciunt reprehenderit consequuntur labore, a ratione velit, sint mollitia quo ducimus fugit nulla animi praesentium placeat dolorum natus architecto?',
+    imageVisionMision: 'https://res.cloudinary.com/dvdb33uyj/image/upload/v1687403791/constructions-4_knczun.jpg',
+}
+
+
+export const AboutHead = {
+    imageHead: 'https://res.cloudinary.com/dvdb33uyj/image/upload/v1687403791/constructions-4_knczun.jpg',
+    text1: 'DATA-Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quae, voluptatum, quod, voluptas quibusdam voluptates quidem voluptatibus quos quia quas dolorum. Quisquam, quae. Quisquam quibusdam, quae quod voluptatum quas. voluptas quibusdam voluptates quidem voluptatibus quos quia quas dolorum. Quisquam, quae. Quisquam quibusdam, quae quod voluptatum quas.',
+    text2: 'DATA-Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quae, voluptatum, quod, voluptas quibusdam voluptates quidem voluptatibus quos quia quas dolorum. Quisquam, quae. Quisquam quibusdam, quae quod voluptatum quas.',
+    text3: 'DATA-Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quae, voluptatum, quod, voluptas quibusdam voluptates quidem voluptatibus quos quia quas dolorum. Quisquam, quae. Quisquam quibusdam, quae quod voluptatum quas.',
+}
+
+export const ContactInformation = {
+    address:'RentalProp Spa Apoquindo 6410 Of.605, Las condes',
+    phone:'+56 9 5046 9097',
+    email:'Contacto@rentalprop.cl',
+    horarioSemana:'Lunes a viernes 8:30 - 18:00hrs.',
+    horarioFinSemana:'Sabado 9:00 - 14:30hrs.',
+    
+}
+
+
+export const ContactFooter = {
+    address:'tFooter-RentalProp Spa Apoquindo 6410 Of.605, Las condes',
+    phone:'tFooter-+56 9 5046 9097',
+    email:'tFooter-Contacto@rentalprop.cl',
+    horarioSemana:'Lunes a viernes 8:30 - 18:00hrs.',
+    horarioFinSemana:'Sabado 9:00 - 14:30hrs.',
+    
+}
+
+
+export const servicesInformation = {
+    cards:[{
+        id:1,
+        icon:'<i class="fa-sharp fa-solid fa-house-circle-check"></i>',
+        title:'ASESORÍA BANCARIA',
+    },{
+        id:2,
+        icon:'<i class="fa-sharp fa-solid fa-handshake-simple"></i>',
+        title:'PUBLICA CON NOSOTROS',
+    },{
+        id:3,
+        icon:'<i class="fa-sharp fa-solid fa-file-contract"></i>',
+        title:'ADMINISTRACIÓN DE ARRRIENDO',
+    },{
+        id:4,
+        icon:'<i class="fa-sharp fa-solid fa-file-signature"></i>',
+        title:'SERVICIOS LEGALES',
+    },
+]
+}
